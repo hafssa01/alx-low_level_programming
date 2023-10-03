34475dfb60374cda74880430c2d91b2df07faaa8
hafssa01 <hafssazaferr@gmail.com>
2023-10-03 05:11:27 -0700

    argv-argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,17 +8,9 @@
  * Return: Always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc >= 1)
-	{
-		printf("Progame name: %s\n", argv[0]);
-	}
-	else
-	{
-		printf("Unable to determine program name.\n");
-	}
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
 
