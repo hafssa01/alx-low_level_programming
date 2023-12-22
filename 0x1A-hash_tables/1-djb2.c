4f87bc5b1dba0f9900eb608e076b6af0994ca66d
hafssa01 <hafssazaferr@gmail.com>
2023-12-22 04:39:34 -0800

    hash_table

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int h;
+	int c;
+
+	h = 5381;
+	while ((c = *str++))
+		h = ((h << 5) + h) + c; /* h * 33 + c */
+
+	return (h);
+}
